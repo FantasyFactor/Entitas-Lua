86478dfeae52ab1aa2d5c9b5e50666cd8ad04488
Lawliet <1577075604@qq.com>
2023-11-30 16:47:13 +0800

    【功能】增加Group

---
+++ Entitas/Group.lua
@@ -0,0 +1,121 @@
+require("Core/Class")
+
+local Delegate = require("Entitas/Delegate")
+
+local Group = Class("Group")
+
+function Group:Ctor(matcher)
+    self.m_Matcher = matcher
+    self.m_Entities = {}
+
+    self.m_EntitiesCache = nil
+
+    self.onEntityAdded = Delegate()    --group, entity, index, component
+    self.onEntityRemoved = Delegate()    --group, entity, index, component
+    self.onEntityUpdated = Delegate()    --group, entity, index, previousComponent, newComponent
+end
+
+local function AddEntitySilently(self, entity)
+    if entity:IsEnable() then
+        local has = self.m_Entities[entity]
+        if has then
+            self.m_EntitiesCache = nil
+        else
+            self.m_Entities[entity] = true
+        end 
+
+        return has
+    end
+
+    return false
+end
+
+local function AddEntity(self, entity, index, component)
+    if self:AddEntitySilently(entity) and self.onEntityAdded ~= nil then
+        self.onEntityAdded(self, entity, index, component)
+    end
+end
+
+local function RemoveEntitySilently(self, entity)
+    local has = self.m_Entities[entity]
+    if has then
+        self.m_Entities[entity] = nil
+        self.m_EntitiesCache = nil
+    end 
+    return false
+end
+
+local function RemoveEntity(self, entity, index, component)
+    local has = self.m_Entities[entity]
+    if has then
+        self.m_Entities[entity] = nil
+        self.m_EntitiesCache = nil
+        
+        if self.onEntityRemoved ~= nil then
+            self.onEntityRemoved(self, entity, index, component)
+        end
+
+    end
+end
+
+local function CopyTo(hashSet, array)
+    for entity, v in pairs(hashSet) do
+        if v == true then
+            table.insert(array, entity)
+        end
+    end
+end
+
+function Group:HandleEntitySilently(entity)
+    if self.m_Matcher:Matches(entity) then
+        AddEntitySilently(self, entity)
+    else
+        RemoveEntitySilently(self, entity)    
+    end
+end
+
+function Group:HandleEntity(entity, index, component)
+    if self.m_Matcher:Matches(entity) then
+        AddEntity(self, entity, index, component)
+    else
+        RemoveEntity(self, entity, index, component)    
+    end
+end
+
+function Group:UpdateEntity(entity, index, previousComponent, newComponent)
+    if self.m_Entities[entity] then
+        if self.onEntityRemoved ~= nil then
+            self.onEntityRemoved(self, entity, index, previousComponent)
+        end
+        if self.onEntityAdded ~= nil then
+            self.onEntityAdded(self, entity, index, newComponent)
+        end
+
+        if self.onEntityUpdated ~= nil then
+            self.onEntityUpdated(self, entity, index, previousComponent, newComponent)
+        end
+    end
+end
+
+function Group:ContainsEntity(entity)
+    return self.m_Entities[entity] == true
+end
+
+function Group:GetEntities(buffer)
+    if buffer == nil then
+        if self.m_EntitiesCache == nil then
+            self.m_EntitiesCache = {}
+            CopyTo(self.m_Entities, self.m_EntitiesCache)
+        end
+        return self.m_EntitiesCache
+    else
+        CopyTo(self.m_Entities, buffer)
+        return buffer
+    end
+end
+
+-- function Group:ToString()
+    
+-- end
+
+return Group
---
+++ Tests/GroupTest.lua
@@ -0,0 +1,136 @@
+luaunit = require('luaunit')
+
+package.path = package.path .. ";?.lua"
+
+local Pools = require("Entitas/Pools")
+local Entity = require("Entitas/Entity")
+local Matcher = require("Entitas/Matcher")
+local Group = require("Entitas/Group")
+
+local function GetTestEntities()
+    local pools = Pools() 
+
+    local results = {}
+
+    for i = 1, 10, 1 do
+        local entity = Entity()
+        entity:Initialize(i, pools)
+
+        local index = i % 4
+        local component = entity:CreateComponent(index)
+
+        entity:AddComponent(index, component)
+
+        table.insert(results, entity)
+    end
+
+    return results
+end
+
+function TestGroupHandleEntitySilently()
+    local entities = GetTestEntities()
+
+    local matcher = Matcher()
+    matcher:AnyOf(1, 3) --entity 1, 3, 5, 7, 9 
+
+    local group = Group(matcher)
+
+    for i, entity in ipairs(entities) do
+        group:HandleEntitySilently(entity)
+    end
+    
+    assert(group.m_Entities[entities[1]])
+    assert(group.m_Entities[entities[3]])
+    assert(group.m_Entities[entities[5]])
+    assert(group.m_Entities[entities[7]])
+    assert(group.m_Entities[entities[9]])
+    assert(not group.m_Entities[entities[6]])
+    assert(not group.m_Entities[entities[10]])
+end
+
+function TestGroupHandleEntity()
+    local entities = GetTestEntities()
+
+    local matcher = Matcher()
+    matcher:AnyOf(1, 3) --entity 1, 3, 5, 7, 9 
+
+    local group = Group(matcher)
+
+    for i, entity in ipairs(entities) do
+        group:HandleEntitySilently(entity)
+    end
+    
+    assert(group.m_Entities[entities[1]])
+    assert(group.m_Entities[entities[3]])
+    assert(group.m_Entities[entities[5]])
+    assert(group.m_Entities[entities[7]])
+    assert(group.m_Entities[entities[9]])
+    assert(not group.m_Entities[entities[6]])
+    assert(not group.m_Entities[entities[10]])
+end
+
+function TestGroupContainsEntity()
+    local entities = GetTestEntities()
+
+    local matcher = Matcher()
+    matcher:AnyOf(1, 3) --entity 1, 3, 5, 7, 9 
+
+    local group = Group(matcher)
+
+    for i, entity in ipairs(entities) do
+        group:HandleEntitySilently(entity)
+    end
+
+    assert(group:ContainsEntity(entities[1]))
+    assert(group:ContainsEntity(entities[3]))
+    assert(group:ContainsEntity(entities[5]))
+    assert(group:ContainsEntity(entities[7]))
+    assert(group:ContainsEntity(entities[9]))
+    assert(not group:ContainsEntity(entities[6]))
+    assert(not group:ContainsEntity(entities[10]))
+end
+
+function TestGroupGetEntities()
+    local entities = GetTestEntities()
+
+    local matcher = Matcher()
+    matcher:AnyOf(1, 3) --entity 1, 3, 5, 7, 9 
+
+    local group = Group(matcher)
+
+    for i, entity in ipairs(entities) do
+        group:HandleEntitySilently(entity)
+    end
+
+    local cacheEnities = group:GetEntities()
+
+    for i, v in ipairs(cacheEnities) do
+        print(v)
+    end
+
+    table.sort(cacheEnities, function(a, b) 
+        return a:GetCreationIndex() < b:GetCreationIndex()
+    end)
+    assert(#cacheEnities == 5)
+    assert(cacheEnities[1] == entities[1])
+    assert(cacheEnities[2] == entities[3])
+    assert(cacheEnities[3] == entities[5])
+    assert(cacheEnities[4] == entities[7])
+    assert(cacheEnities[5] == entities[9])
+
+    local enityBuffer = {}
+
+    group:GetEntities(enityBuffer)
+
+    table.sort(enityBuffer, function(a, b) 
+        return a:GetCreationIndex() < b:GetCreationIndex()
+    end)
+    assert(#enityBuffer == 5)
+    assert(enityBuffer[1] == entities[1])
+    assert(enityBuffer[2] == entities[3])
+    assert(enityBuffer[3] == entities[5])
+    assert(enityBuffer[4] == entities[7])
+    assert(enityBuffer[5] == entities[9])
+end
+
+os.exit(luaunit.LuaUnit.run())
