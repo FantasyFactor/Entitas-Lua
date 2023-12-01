019edacbb444c05bc8e824fd49414eed88c8d187
Lawliet <1577075604@qq.com>
2023-12-01 15:46:53 +0800

    【优化】Delegate回调支持获取监听对象 【功能】增加获取Count的接口

--- Entitas/Delegate.lua
+++ Entitas/Delegate.lua
@@ -4,43 +4,54 @@ local DelegateImplementation = {}
 
 local function Invoke(self, ...)
     for i = 1, self.m_Count, 1 do
-        self.m_InvokeList[i](...)
-    end
-end
-
-local function AddDelegate(self, ...)
-    for _, v in pairs({...}) do
-        if type(v) == "function" then
-            table.insert(self.m_InvokeList, v)
-            self.m_Count = self.m_Count + 1
-        else
-            error(string.format("Can't add not function type:%s", type(v))) 
+        local value = self.m_InvokeList[i]
+        if value ~= nil and value.callback ~= nil then
+            value.callback(value.target, ...)
         end
     end
 end
 
-local function RemoveDelegate(self, ...)
-    for _, v in pairs({...}) do
-        if type(v) == "function" then
-            for i = 1, self.m_Count, 1 do
-                if self.m_InvokeList[i] == v then
-                    table.remove(self.m_InvokeList, i)
+local function AddDelegate(self, target, ...)
+    local n = select("#", ...)
+    for i = 1, n, 1 do
+        local callback = select(i, ...)
+        if type(callback) == "function" then
+            local value = {target = target, callback = callback}
+            table.insert(self.m_InvokeList, value)
+            self.m_Count = self.m_Count + 1
+        else
+            error(string.format("Can't add not function type:%s", type(callback))) 
+        end
+    end
+end
+
+local function RemoveDelegate(self, target, ...)
+    local n = select("#", ...)
+    for i = 1, n, 1 do
+        local callback = select(i, ...)
+        if type(callback) == "function" then
+            for j = 1, self.m_Count, 1 do
+                local value = self.m_InvokeList[j]
+                if value.target == target and value.callback == callback then
+                    table.remove(self.m_InvokeList, j)
                     self.m_Count = self.m_Count - 1
                     break
                 end
             end
         else
-            error(string.format("Can't add not function type:%s", type(v))) 
+            error(string.format("Can't add not function type:%s", type(callback))) 
         end
     end
 end
 
+local function GetCount(self)
+    return self.m_Count
+end
+
 setmetatable(Delegate, {
-    __call = function(self, ...)
+    __call = function()
         local delegate = {m_InvokeList = {}, m_Count = 0}
         setmetatable(delegate, DelegateImplementation)
-        --print(delegate)
-        delegate:AddDelegate(...)
         return delegate
     end
 })
@@ -56,6 +67,7 @@ end
 DelegateImplementation.__call = Invoke
 DelegateImplementation.AddDelegate = AddDelegate
 DelegateImplementation.RemoveDelegate = RemoveDelegate
+DelegateImplementation.GetCount = GetCount
 DelegateImplementation.Invoke = Invoke
 
 return Delegate
--- Tests/DelegateTest.lua
+++ Tests/DelegateTest.lua
@@ -5,25 +5,25 @@ package.path = package.path .. ";?.lua"
 local Delegate = require("Entitas/Delegate")
 
 
-local function delegate1(value)
+local function delegate1(self, value)
     
 end
 
-local function delegate2(value)
+local function delegate2(self, value)
     
 end
 
 function TestNewDelegate()
     local d1 = Delegate()
-    local d2 = Delegate(delegate1)
-    local d3 = Delegate(delegate1, delegate2)
+    assert(d1.m_InvokeList ~= nil)
+    assert(d1.m_Count == 0)
 end
 
 function TestAddDelegate()
     local d1 = Delegate()
 
-    d1:AddDelegate(delegate1)
-    d1:AddDelegate(delegate2)
+    d1:AddDelegate(nil, delegate1)
+    d1:AddDelegate(nil, delegate2)
 
     assert(d1.m_Count == 2)
 end
@@ -33,10 +33,10 @@ function TestCallDelegate()
 
     local n = 0
 
-    d1:AddDelegate(function (value)
+    d1:AddDelegate(nil, function (self, value)
         n = n + value
     end)
-    d1:AddDelegate(function (value)
+    d1:AddDelegate(nil, function (self, value)
         n = n* value
     end)
 
@@ -48,15 +48,15 @@ end
 function TestRemoveDelegate()
     local d1 = Delegate()
 
-    d1:AddDelegate(delegate1)
-    d1:AddDelegate(delegate2)
+    d1:AddDelegate(nil, delegate1)
+    d1:AddDelegate(nil, delegate2)
 
-    assert(d1.m_Count == 2)
+    assert(d1:GetCount() == 2)
 
-    d1:RemoveDelegate(delegate1)
-    d1:RemoveDelegate(delegate2)
+    d1:RemoveDelegate(nil, delegate1)
+    d1:RemoveDelegate(nil, delegate2)
 
-    assert(d1.m_Count == 0)
+    assert(d1:GetCount() == 0)
 end
 
 os.exit(luaunit.LuaUnit.run())
